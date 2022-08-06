18036c5b993ca61141416122d22a773fb3cd9c07
Mihai <mihai.iusztin@gmail.com>
2022-08-06 15:49:48 +0300

    first version

---
+++ app.js
@@ -0,0 +1,36 @@
+function displayTeams(teams) {
+  //   console.log('display', teams);
+
+  var teamsHTML = '';
+  teams.forEach((team) => {
+    // console.info(team);
+    // transforma in html
+    teamsHTML += `
+     <tr>
+        <td>${team.promotion}</td>
+        <td>${team.members}</td>
+        <td>${team.name}</td>
+        <td>
+        <a href="${team.url}">open</a>
+        </td>
+        <td>x e</td>
+   </tr>`;
+  });
+  //afisare
+  //   console.warn(teamsHTML);
+  document.querySelector('table tbody').innerHTML = teamsHTML;
+}
+
+function loadTeams() {
+  fetch('data/teams.json')
+    .then(function (r) {
+      // console.info(r);
+      return r.json();
+    })
+    .then(function (teams) {
+      //   console.warn('r', teams);
+      displayTeams(teams);
+    });
+}
+
+loadTeams();
