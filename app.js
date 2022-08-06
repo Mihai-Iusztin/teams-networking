b656d54eefcb82be5d5b172f20969873ceceb7ae
Mihai <mihai.iusztin@gmail.com>
2022-08-06 16:45:24 +0300

    extract getTeamHTML

--- app.js
+++ app.js
@@ -1,3 +1,16 @@
+function getTeamHTML(team) {
+  return `
+  <tr>
+     <td>${team.promotion}</td>
+     <td>${team.members}</td>
+     <td>${team.name}</td>
+     <td>
+     <a href="${team.url}">open</a>
+     </td>
+     <td>x e</td>
+</tr>`;
+}
+
 function displayTeams(teams) {
   //   console.log('display', teams);
 
@@ -5,16 +18,17 @@ function displayTeams(teams) {
   teams.forEach((team) => {
     // console.info(team);
     // transforma in html
-    teamsHTML += `
-     <tr>
-        <td>${team.promotion}</td>
-        <td>${team.members}</td>
-        <td>${team.name}</td>
-        <td>
-        <a href="${team.url}">open</a>
-        </td>
-        <td>x e</td>
-   </tr>`;
+    teamsHTML += getTeamHTML(team);
+    //   `
+    //    <tr>
+    //       <td>${team.promotion}</td>
+    //       <td>${team.members}</td>
+    //       <td>${team.name}</td>
+    //       <td>
+    //       <a href="${team.url}">open</a>
+    //       </td>
+    //       <td>x e</td>
+    //  </tr>`;
   });
   //afisare
   //   console.warn(teamsHTML);
