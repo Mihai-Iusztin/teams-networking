2575c454803bc2fef56ed8de6302e852505c2d70
Mihai <mihai.iusztin@gmail.com>
2022-08-06 17:07:47 +0300

    use map

--- app.js
+++ app.js
@@ -14,25 +14,32 @@ function getTeamHTML(team) {
 function displayTeams(teams) {
   //   console.log('display', teams);
 
-  var teamsHTML = '';
-  teams.forEach((team) => {
-    // console.info(team);
-    // transforma in html
-    teamsHTML += getTeamHTML(team);
-    //   `
-    //    <tr>
-    //       <td>${team.promotion}</td>
-    //       <td>${team.members}</td>
-    //       <td>${team.name}</td>
-    //       <td>
-    //       <a href="${team.url}">open</a>
-    //       </td>
-    //       <td>x e</td>
-    //  </tr>`;
+  // var teamsHTML = '';
+  // teams.forEach((team) => {
+  // console.info(team);
+  // transforma in html
+  // teamsHTML += getTeamHTML(team);
+  //   `
+  //    <tr>
+  //       <td>${team.promotion}</td>
+  //       <td>${team.members}</td>
+  //       <td>${team.name}</td>
+  //       <td>
+  //       <a href="${team.url}">open</a>
+  //       </td>
+  //       <td>x e</td>
+  //  </tr>`;
+  // });
+
+  var teamsHTML = teams.map(function (team) {
+    console.info(team);
+    return getTeamHTML(team);
   });
+  console.warn('r', teamsHTML);
+
   //afisare
   //   console.warn(teamsHTML);
-  document.querySelector('table tbody').innerHTML = teamsHTML;
+  document.querySelector('table tbody').innerHTML = teamsHTML.join('');
 }
 
 function loadTeams() {
